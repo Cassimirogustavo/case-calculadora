b0085840e7c95f4cce2bd1190881a626ac935304
Gustavo Cassimiro <142855595+Cassimirogustavo@users.noreply.github.com>
2024-07-18 00:41:13 -0300

    Update Operacoes.cs

--- Operacoes.cs
+++ Operacoes.cs
@@ -1,12 +1,11 @@
-﻿using System;
 namespace Calculadora
 {
     public class Operacoes
     {
-        public int valorA {get;set;}
+        // Propriedades para armazenar os valores e operador da operação
+        public int valorA { get; set; }
         public char operador { get; set; }
         public int valorB { get; set; }
         public decimal resultado { get; set; }
-
     }
 }
